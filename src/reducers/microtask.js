684e851866532af1d22c8fb1a8f4dd022e001536
Евгений Жиров <girov189916@gmail.com>
2021-01-21 16:11:38 +0300

    last change

--- src/reducers/microtask.js
+++ src/reducers/microtask.js
@@ -1,10 +1,8 @@
-import { act } from 'react-dom/test-utils';
 import {ADD_MICRO_TASK,CHANGE_MICRO_TASK,DELETE_MICRO_TASK,CHANGE_PLACE_MT} from '../constants/actions'
 
 const initialState = {
   tasks_arr: [],
   tasks_obj: {},
-  proccesing:false
 }
 
 export const microtask = (state = initialState,action)=> {
@@ -16,7 +14,6 @@ export const microtask = (state = initialState,action)=> {
           return {
               ...state,
               tasks_arr:[...state.tasks_arr,action.payload.id],
-              
           };
       case CHANGE_MICRO_TASK:
         state.tasks_obj[action.payload.id][action.payload.nameProp] = action.payload.value
@@ -25,7 +22,7 @@ export const microtask = (state = initialState,action)=> {
             task_obj:{...state.task_obj}
           }
       case CHANGE_PLACE_MT:
-        
+      
           const result = [...state.tasks_arr];
 
           const firstElement =  Object.values(state.tasks_obj).filter((elem)=>(String(elem.id_task)===String(action.payload.source.droppableId)))[Number(action.payload.source.index)]
