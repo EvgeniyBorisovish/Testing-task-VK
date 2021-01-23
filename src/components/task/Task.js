7963dac44cd84aec20c2e2e38d0c05cbe2091f7b
Евгений Жиров <girov189916@gmail.com>
2021-01-23 17:28:09 +0300

    chnage

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React,{useState} from 'react'
 import {useSelector,useDispatch} from 'react-redux'
 import {ADD_MICRO_TASK,DELETE_TASK} from '../../constants/actions'
 import {nanoid} from 'nanoid'
@@ -9,10 +9,10 @@ import  Addtask  from "../addtask";
 import clearSvg from "../../images/clear.svg";
 
 export const Task = ({id_task,provided})=>{
+const [visibleMicrotasks,setVisible] = useState(true)
 const dispath = useDispatch()
 const data_obj = useSelector((state)=>(state.task.tasks_obj[id_task]))
 const tasks_arr = useSelector((state)=>(Object.values(state.microtask.tasks_obj).filter((task)=>(task.id_task===id_task))))
-console.log(tasks_arr.length)
 
 const addMicroTaskHandler = (actionType,value)=>{
   value = value.trim()
@@ -22,19 +22,28 @@ const addMicroTaskHandler = (actionType,value)=>{
 const handDelTask = ()=>{
   dispath({type:DELETE_TASK,payload:id_task})
 }
+const shangeVisible = ()=>{
+  setVisible(!visibleMicrotasks)
 
+}
 return (<div className="task">
         <div className="task-header">
           
-          <b className="task-header__captionTask">{data_obj.caption}</b>{ tasks_arr.length>0  && <span>Раскрыть</span>}
-          <div className="task-header__btnClose" onClick={handDelTask} ><img src={clearSvg}></img></div>
+        <b className="task-header__captionTask">{data_obj.caption}{ tasks_arr.length>0  && <span className="task-header__showMicrotask" onClick={shangeVisible}> &nbsp;&nbsp; {visibleMicrotasks?"Cкрыть":"Раскрыть"} </span>}</b>
+        <div className="task-header__btnClose" onClick={handDelTask} ><img src={clearSvg}></img></div>
         </div>
+        {
+          visibleMicrotasks
+          &&
         <div className="task-microtasks">
               {tasks_arr.map(({id,text,id_task},index)=>{
                 return(<Microtask id={id} text={text} key={String(id)} index={index}/>)
               })}
         {provided.placeholder}
         </div>
+
+        }
+        
         
         <Addtask getAction={addMicroTaskHandler.bind(null,ADD_MICRO_TASK)} textPlaceholder={"Введите текст  микрозадача"}/>
         </div>);
